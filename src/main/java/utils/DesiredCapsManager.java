9996d81e677825a50de03e3ff8d87d640e6ab6b7
arturpoturyan <adminkiller123>
2019-01-15 11:19:56 +0400

    Delete comments

--- src/main/java/utils/DesiredCapsManager.java
+++ src/main/java/utils/DesiredCapsManager.java
@@ -10,22 +10,17 @@ import org.testng.annotations.BeforeClass;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.concurrent.TimeUnit;
 
 public class DesiredCapsManager {
-    protected  AndroidDriver<MobileElement> driver;
+    protected AndroidDriver<MobileElement> driver;
 
-//    public DesiredCapsManager() {
-//        setUp();
-//    }
+    public DesiredCapsManager() {
+        setUp();
+    }
 
     @BeforeClass
     public void setUp() {
-//        File filePath = new File(System.getProperty("user.dir"));
-//        File appDir = new File(filePath, "/app");
-//        File app = new File(appDir, "/Users/arturpoturyan/Downloads/picsart-10.8-unsigned-117-signed.apk");
-//        capabilities.setCapability("app", app.getAbsolutePath());
-//        capabilities.setCapability("automationName", AutomationName.APPIUM);
+
         String apk = "/Users/arturpoturyan/Downloads/11.3_ON.apk";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(MobileCapabilityType.UDID, "a8e4926b");
@@ -36,35 +31,21 @@ public class DesiredCapsManager {
         capabilities.setCapability("app", apk);
         capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE,
                 "com.picsart.studio");
-      //  capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Uiautomator2");
         capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,
                 "com.socialin.android.photo.picsinphoto.MainPagerActivity");
 
         try {
-            driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+            driver = new AndroidDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
 
-//        try {
-//            Capabilities caps = driver.getCapabilities();
-//
-//            assertEquals(AutomationName.APPIUM, caps.getCapability(MobileCapabilityType.AUTOMATION_NAME));
-//            assertEquals(MobilePlatform.ANDROID, caps.getCapability(MobileCapabilityType.PLATFORM_NAME));
-//            assertNotEquals(null, caps.getCapability(MobileCapabilityType.DEVICE_NAME));
-//            assertEquals(apk, caps.getCapability(MobileCapabilityType.APP));
-//        } finally {
-//            driver.quit();
-//        }
-//        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
 
 
     @AfterClass(alwaysRun = true)
     public void tearDown() {
-        //if (driver != null) {
         driver.quit();
-        // }
 
     }
 }
